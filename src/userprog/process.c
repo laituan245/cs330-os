06b9d67ad5da8bdf96a7a4b9a7b9973a2705df39
Nham Le <nhamlevan@gmail.com>
2016-04-04 09:46:55 +0400

    arg passing
    havent committed changes in process_wait()

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -17,9 +17,9 @@
 #include "threads/palloc.h"
 #include "threads/thread.h"
 #include "threads/vaddr.h"
-
+#include "threads/malloc.h"
 static thread_func start_process NO_RETURN;
-static bool load (const char *cmdline, void (**eip) (void), void **esp);
+static bool load (const char *cmdline, void (**eip) (void), void **esp, char** save_ptr);
 
 /* Starts a new thread running a user program loaded from
    FILENAME.  The new thread may be scheduled (and may even exit)
@@ -54,12 +54,15 @@ start_process (void *f_name)
   struct intr_frame if_;
   bool success;
 
+  char *args;
+  file_name = strtok_r(file_name, " ", &args);
+
   /* Initialize interrupt frame and load executable. */
   memset (&if_, 0, sizeof if_);
   if_.gs = if_.fs = if_.es = if_.ds = if_.ss = SEL_UDSEG;
   if_.cs = SEL_UCSEG;
   if_.eflags = FLAG_IF | FLAG_MBS;
-  success = load (file_name, &if_.eip, &if_.esp);
+  success = load (file_name, &if_.eip, &if_.esp, &args);
 
   /* If load failed, quit. */
   palloc_free_page (file_name);
@@ -90,6 +93,7 @@ start_process (void *f_name)
 int
 process_wait (tid_t child_tid UNUSED) 
 {
+
   return -1;
 }
 
@@ -197,7 +201,7 @@ struct Elf32_Phdr
 #define PF_W 2          /* Writable. */
 #define PF_R 4          /* Readable. */
 
-static bool setup_stack (void **esp);
+static bool setup_stack (void **esp, char* file_name, char** save_ptr);
 static bool validate_segment (const struct Elf32_Phdr *, struct file *);
 static bool load_segment (struct file *file, off_t ofs, uint8_t *upage,
                           uint32_t read_bytes, uint32_t zero_bytes,
@@ -208,7 +212,7 @@ static bool load_segment (struct file *file, off_t ofs, uint8_t *upage,
    and its initial stack pointer into *ESP.
    Returns true if successful, false otherwise. */
 bool
-load (const char *file_name, void (**eip) (void), void **esp) 
+load (const char *file_name, void (**eip) (void), void **esp, char **save_ptr) 
 {
   struct thread *t = thread_current ();
   struct Elf32_Ehdr ehdr;
@@ -304,7 +308,7 @@ load (const char *file_name, void (**eip) (void), void **esp)
     }
 
   /* Set up stack. */
-  if (!setup_stack (esp))
+  if (!setup_stack (esp, file_name, save_ptr))
     goto done;
 
   /* Start address. */
@@ -429,7 +433,7 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage,
 /* Create a minimal stack by mapping a zeroed page at the top of
    user virtual memory. */
 static bool
-setup_stack (void **esp) 
+setup_stack (void **esp, char* file_name, char** save_ptr) 
 {
   uint8_t *kpage;
   bool success = false;
@@ -440,9 +444,49 @@ setup_stack (void **esp)
       success = install_page (((uint8_t *) PHYS_BASE) - PGSIZE, kpage, true);
       if (success)
         *esp = PHYS_BASE;
-      else
+      else{
         palloc_free_page (kpage);
+        return success;
+      }
+        
     }
+
+  char *token;
+  char **argv = malloc(100*sizeof(char *));
+  int i=0;
+  int argc =0;
+
+  //Push args to the stack
+  for(token = (char *)file_name; token !=NULL; token = strtok_r(NULL, " ", save_ptr)){
+    *esp-= strlen(token) + 1;
+    argv[argc] = *esp;
+    argc++;
+    memcpy(*esp, token, strlen(token)+1);
+  }
+  argv[argc] = 0;
+  ////word-align
+  i = (size_t) *esp%4;
+  if(i){
+    *esp-=i;
+    memcpy(*esp, &argv[argc], i);
+  }
+  //Push addresses of argv to the stack
+  for(i=argc; i>=0; i--){
+    *esp-= sizeof(char *);
+    memcpy(*esp, &argv[i], sizeof(char *));
+  }
+  //push argv
+  token = *esp;
+  *esp -= sizeof(char **);
+  memcpy(*esp, &token, sizeof(char **));
+  //push argc
+  *esp-= sizeof(int);
+  memcpy(*esp, &argc, sizeof(int));
+  //push return address
+  *esp -= sizeof(void *);
+  memcpy(*esp, &argv[argc], sizeof(void *));
+  //free memory
+  free(argv);
   return success;
 }
 
